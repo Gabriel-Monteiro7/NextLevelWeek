b1be61a1e61b2daba025322a028edf6b8f3ad880
GabrielMonteiro <gabrielmont713gmail.com>
2020-06-21 00:10:51 -0300

    Ajuste no arquivo do heroku

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,11 +1,8 @@
 import knex from "knex";
-import path from "path";
 
 const connection = knex({
-  client: "sqlite3",
-  connection: {
-    filename: path.resolve(__dirname, "database.sqlite"),
-  },
+  client: "postgresql",
+  connection: process.env.DATABASE_URL,
   useNullAsDefault: true,
 });
 
